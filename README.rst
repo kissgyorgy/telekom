9423e183077336915678d59cfe7837bfcf2cfbae
Kiss György <kissgyorgy@me.com>
2016-02-13 19:34:33 +0100

    Updated README added more documentation and screenshot.

--- README.rst
+++ README.rst
@@ -1,4 +1,8 @@
-Command line application for http://www.telekom.hu.
+Command line application for interacting with the http://www.telekom.hu website.
 
 
 Currently it can only login and get the mobile data usage limit.
+If you wrap it with `Platypus <http://www.sveinbjorn.org/platypus>`_ and call limit command with
+the ``--platypus`` option, it will show a notification on OS X.
+
+.. image:: notification.png
--- telekom.py
+++ telekom.py
@@ -16,9 +16,46 @@ SCRIPT_DIR = os.path.expanduser('~/.telekom')
 SESSION_FILE = os.path.join(SCRIPT_DIR, 'session.pickle')
 
 
+def make_session(login, password):
+    """Log in and store the pickled requests session in file."""
+    session = requests.Session()
+    session.post(LOGIN_URL, data={"service": "TF-pwd", "logonId": login, "password": password})
+    with open(SESSION_FILE, 'wb') as f:
+        pickle.dump(session, f)
+
+
+def download_page():
+    """Restore the session from the pickled file and download the balance page."""
+    with open(SESSION_FILE, 'rb') as f:
+        session = pickle.load(f)
+    return session.get(BALANCE_URL).content
+
+
+def get_limit_from_page(html):
+    """Find the var element in the downloaded page which contains the limit in bytes."""
+    root = etree.HTML(html)
+    # There are multiple <var class="limit"> elements, we are interested in the summary only
+    size_in_bytes = root.xpath(LIMIT_ELEMENT)[0]
+    return size_in_bytes
+
+
+def get_balance():
+    html = download_page()
+    size_in_bytes = get_limit_from_page(html)
+    human_readable = humanize.naturalsize(int(size_in_bytes), binary=True)
+    return "Balance: {} ({})".format(size_in_bytes, human_readable)
+
+
+def get_platypus_balance():
+    """Makes OS X notification if wrapped with Platypus.
+    See: http://www.sveinbjorn.org/files/manpages/PlatypusDocumentation.html#10-2
+    """
+    return 'NOTIFICATION:' + get_balance()
+
+
 @click.group()
 def telekom():
-    pass
+    """Command line application for interacting with the http://www.telekom.hu website."""
 
 
 @telekom.command()
@@ -35,40 +72,9 @@ def login(login, password):
         click.echo("Success!")
 
 
-def make_session(login, password):
-    session = requests.Session()
-    session.post(LOGIN_URL, data={"service": "TF-pwd", "logonId": login, "password": password})
-    with open(SESSION_FILE, 'wb') as f:
-        pickle.dump(session, f)
-
-
-def download_page():
-    with open(SESSION_FILE, 'rb') as f:
-        session = pickle.load(f)
-    return session.get(BALANCE_URL).content
-
-
-def get_limit_from_page(html):
-    root = etree.HTML(html)
-    # There are multiple <var class="limit"> elements, we are interested in the summary only
-    size_in_bytes = root.xpath(LIMIT_ELEMENT)[0]
-    return size_in_bytes
-
-
-def get_balance():
-    html = download_page()
-    size_in_bytes = get_limit_from_page(html)
-    human_readable = humanize.naturalsize(int(size_in_bytes), binary=True)
-    return "Balance: {} ({})".format(size_in_bytes, human_readable)
-
-
-def get_platypus_balance():
-    return 'NOTIFICATION:' + get_balance()
-
-
-@telekom.command(name="limit")
+@telekom.command()
 @click.option('--platypus', is_flag=True)
-def print_limit(platypus=False):
+def limit(platypus=False):
     """Shows mobile data limit balance left in human readable form and bytes."""
     click.echo(get_platypus_balance() if platypus else get_balance())
 
